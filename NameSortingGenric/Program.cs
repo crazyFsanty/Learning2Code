5f07530948ad3ef2b996c64ed7abe2b28a88f6a1
क्रेZYसैनtY <crazysanty@gmail.com>
2016-11-21 15:25:20 +0530

    Add files via upload

---
+++ NameSortingGenric/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace NameSorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,j;
+            Console.Write("How many names You want to enter: ");
+            j = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("\nPlease Enter"+" "+j+" "+"Names");
+            string[] nam = new string[j];
+            for (i=0; i<j; i++)
+            {
+                Console.Write("\nplease enter"+" "+(i+1)+" "+"Name: ");
+                nam[i] = Convert.ToString(Console.ReadLine());      
+            }
+            Console.WriteLine("\n--------Sorted Names----------\n");
+            Array.Sort(nam);
+            nam[j].ToUpper();
+            foreach (string str in nam)
+            {
+                Console.WriteLine(str + "\n");
+            }
+        }
+    }
+}
