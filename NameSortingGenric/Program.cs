6f89fcc5eeb2c3f7785cb735aa8af111984d36dc
क्रेZYसैनtY <crazysanty@gmail.com>
2016-11-21 19:53:55 +0530

    Update Program.cs

--- NameSortingGenric/Program.cs
+++ NameSortingGenric/Program.cs
@@ -1,26 +1,25 @@
-﻿using System;
+using System;
 namespace NameSorting
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int i,j;
+            int i, j;
             Console.Write("How many names You want to enter: ");
             j = Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine("\nPlease Enter"+" "+j+" "+"Names");
+            Console.WriteLine("\nPlease Enter" + " " + j + " " + "Names");
             string[] nam = new string[j];
-            for (i=0; i<j; i++)
+            for (i = 0; i < j; i++)
             {
-                Console.Write("\nplease enter"+" "+(i+1)+" "+"Name: ");
-                nam[i] = Convert.ToString(Console.ReadLine());      
+                Console.Write("\nPlease enter" + " " + (i + 1) + " " + "Name: ");
+                nam[i] = Convert.ToString(Console.ReadLine());
             }
             Console.WriteLine("\n--------Sorted Names----------\n");
             Array.Sort(nam);
-            nam[j].ToUpper();
             foreach (string str in nam)
             {
-                Console.WriteLine(str + "\n");
+                Console.WriteLine(str.ToUpper() + "\n");
             }
         }
     }
