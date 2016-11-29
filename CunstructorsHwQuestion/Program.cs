cffc1fb8fa28b294e07b30077c9f6908bb42f041
क्रेZYसैनtY <crazysanty@gmail.com>
2016-11-29 18:21:58 +0530

    Add files via upload

---
+++ CunstructorsHwQuestion/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorQuestionSelfTry
+{
+    //class Circle
+    class Circle
+    {
+        //state or data
+        double pi,r;
+        //Construct type=Parameterised
+        public Circle(double Pi,double R)
+        {
+            pi = Pi;
+            r = R;
+        }
+        //Method 1
+        public void AreaOfCircle() 
+        {
+            Console.WriteLine("Area Of Circle: "+(pi*r*r));
+        }
+        //Method 2
+        public void PerimeterOfcircle() 
+        {
+            Console.WriteLine("Perimeter of circle: "+(2*pi*r));
+        }
+       
+    }
+    //Main Program 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creating a class instance
+            Circle c1 = new Circle(3.14, 5);
+            c1.AreaOfCircle();
+            c1.PerimeterOfcircle();
+           
+        }
+    }
+}
